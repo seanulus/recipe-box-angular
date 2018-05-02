0f1ab0856644e46ec9532f957e6f4baf02d958de
Cory West-Forbes and Sean McDermott <cory@>
2018-05-02 09:00:27 -0700

    add direct for child recipe list from master recipe list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Recipe } from './models/recipe.model'
 export class AppComponent {
   title = 'app';
   currentFocus: string = 'Store Your Recipes'
-  recipes: Recipe[] = [
+  masterRecipeList: Recipe[] = [
     new Recipe('Macaroni and Cheese', ['Macaroni', 'Cheddar Cheese', 'Milk', 'Flour', 'Butter', 'Salt'], 'First, start boiling some water on the stove.', 1),
     new Recipe('Cookie', ['Cookie dough'], 'Throw it in the oven', 2)
   ]
